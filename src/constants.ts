11ca827b98900f3382a6ca62e0ebd0d612444c23
Jose Joya <jose.joya@globant.com>
2024-10-03 10:19:16 -0500

    feat: added favicon and astro config to access static resources correctly

--- src/constants.ts
+++ src/constants.ts
@@ -28,5 +28,5 @@ export const INIT_X_SPEED = 3;
 export const BOX_OFFSET = 50;
 export const DEFAULT_DEBRIS: Debris = { x: 0, y: 0, width: 0, color: "" };
 
-export const BG_SOUND = new Audio("/sounds/main-loop.wav");
-export const HIT_SOUND = new Audio("/sounds/click.wav");
+export const BG_SOUND = new Audio("/stacker/sounds/main-loop.wav");
+export const HIT_SOUND = new Audio("/stacker/sounds/click.wav");
